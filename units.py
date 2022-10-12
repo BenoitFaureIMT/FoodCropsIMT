908e7ce426e85f9eccc3a87281311e31f23c5a8c
BenoitFaureIMT <Dovahkiin2601>
2022-10-12 17:36:13 +0200

    absctract unit

--- units.py
+++ units.py
@@ -1,4 +1,8 @@
-class Unit: 
+from abc import abstractclassmethod
+
+from abc import ABC
+
+class Unit(ABC): 
     def __init__(self, id:int, name:str):
         self.id = id
         self.name  = name
