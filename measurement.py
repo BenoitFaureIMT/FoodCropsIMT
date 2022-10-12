c790de9db6f0110bd41e09cac00b596bae2dea5f
Antoine Mathieu <antoine04.mathieu@gmail.com>
2022-10-12 17:20:09 +0200

    put attribut in private

--- measurement.py
+++ measurement.py
@@ -1,6 +1,6 @@
 class Measurement:
     def __init__(self,year: int,value: float,timeperiodld: float,timeperiodDescr: str):
-        self.year = year
-        self.value = value
-        self.timeperiodld = timeperiodld
-        self.timeperiodDescr = timeperiodDescr
+        self.__year = year
+        self.__value = value
+        self.__timeperiodld = timeperiodld
+        self.__timeperiodDescr = timeperiodDescr
