a71c22c9170b1d8b5064ba84a4e83644bb9a2cf9
BenoitFaureIMT <Dovahkiin2601>
2022-10-12 17:30:41 +0200

    fix units

--- units.py
+++ units.py
@@ -13,18 +13,18 @@ class Price(Unit):
 
 class Weight(Unit):
     def __init__(self, id:int, multiplier:float):
-        super(Price, self).__init__(id, "Weight")
+        super(Weight, self).__init__(id, "Weight")
         self.__multiplier = multiplier
 
 class Surface(Unit):
     def __init__(self, id:int):
-        super(Price, self).__init__(id, "Surface")
+        super(Surface, self).__init__(id, "Surface")
 
 class Count(Unit):
     def __init__(self, id:int, what:str):
-        super(Price, self).__init__(id, "Count")
+        super(Count, self).__init__(id, "Count")
         self.__what = what
 
 class Ratio(Unit):
     def __init__(self, id:int):
-        super(Price, self).__init__(id, "Ratio")
+        super(Ratio, self).__init__(id, "Ratio")
