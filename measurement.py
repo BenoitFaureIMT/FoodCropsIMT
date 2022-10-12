9d8c10d5dd2f209f7068934bc45fe9e603dedbaf
BenoitFaureIMT <Dovahkiin2601>
2022-10-12 18:01:15 +0200

    Measurment

--- measurement.py
+++ measurement.py
@@ -1,6 +1,13 @@
+from commodity import Commodity
+from indicator import Indicator
+
+
 class Measurement:
     def __init__(self,year: int,value: float,timeperiodld: float,timeperiodDescr: str):
         self.__year = year
         self.__value = value
         self.__timeperiodld = timeperiodld
         self.__timeperiodDescr = timeperiodDescr
+    
+    def measurment(id: int, year: int, value: float, timeperiodld: float, timeperiodDesc: str, commodity: Commodity, indicator: Indicator):
+        return None
