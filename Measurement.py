19e746d82717f19c1c5eba2892f0951ebd25c633
Antoine Mathieu <antoine04.mathieu@gmail.com>
2022-10-12 17:10:10 +0200

    Mesurement constructor

---
+++ Measurement.py
@@ -0,0 +1,6 @@
+class Measurement:
+    def __init__(self,year: int,value: float,timeperiodld: float,timeperiodDescr: str):
+        self.year = year
+        self.value = value
+        self.timeperiodld = timeperiodld
+        self.timeperiodDescr = timeperiodDescr
