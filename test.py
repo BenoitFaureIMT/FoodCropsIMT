e571f87380e20af16faf6c6f2995ce2dee09bb50
NathanOD <nathan.odic@mines-ales.org>
2022-11-09 17:40:28 +0100

    update progress bar

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-
-volume = ['Carloads originated', '"1,000 liters"', 'Gallons']
-price = ['Index (1984=100)', 'Dollars','Cents']
-weight = {'Million bushels': 1000000, 'Bushels': 1, 'Bushel': 1, 'Cwt': 1, '"1,000 metric tons"': 1000, 'Million metric tons': 1000000, '"1,000 tons"': 1000, 'Tons': 1, 'Ton': 1, 'Pound': 1, 'Short ton': 1, 'Metric tons': 1, 'Million animal units': 1000000}
-surface = ['Million acres', '"1,000 acres"', '"1,000 hectare"', 'Acre','Hectare']
-count = []
-ratio = ['Ratio']
-ratioUnit = {'Dollars per bushel': ['Dollars','Bushel'], 'Dollars per cwt': ['Dollars','Cwt'], 'Bushels per acre': ['Bushels','Acre'], 'Tons per acre': ['Tons','Acre'], 'Dollars per ton': ['Dollars','Ton'], 'Cents per pound': ['Cents','Pound'], 'Dollars per short ton': ['Dollars','Short ton'], 'Metric tons per hectare': ['Metric tons','Hectare']}
-
-
+import pandas as pd
+dataset = pd.read_csv('FeedGrains.csv')
+tot = dataset.shape[0]
+width = 50
+for index, __ in dataset.iterrows():
+    percent = 100.0*index/tot
+    left = width*percent/100
+    right = width-left
+    print("\r[", '*'*int(left), ' '*int(right), "]", round(percent), '%', sep="", end="", flush=True)
