108f923ac078e4788f28959a5d44cf69e818413f
NathanOD <nathan.odic@mines-ales.org>
2022-11-09 13:59:43 +0100

    update test

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 
-volume = ['Carloads originated', '"1,000 liters"', 'Gallons', ]
-price = ['Index (1984=100)', ]
-weight = []
-surface = ['Million acres', '"1,000 acres"', '"1,000 hectare"',  ]
+volume = ['Carloads originated', '"1,000 liters"', 'Gallons']
+price = ['Index (1984=100)', 'Dollars','Cents']
+weight = {'Million bushels': 1000000, 'Bushels': 1, 'Bushel': 1, 'Cwt': 1, '"1,000 metric tons"': 1000, 'Million metric tons': 1000000, '"1,000 tons"': 1000, 'Tons': 1, 'Ton': 1, 'Pound': 1, 'Short ton': 1, 'Metric tons': 1, 'Million animal units': 1000000}
+surface = ['Million acres', '"1,000 acres"', '"1,000 hectare"', 'Acre','Hectare']
 count = []
-ratio = []
+ratio = ['Ratio']
+ratioUnit = {'Dollars per bushel': ['Dollars','Bushel'], 'Dollars per cwt': ['Dollars','Cwt'], 'Bushels per acre': ['Bushels','Acre'], 'Tons per acre': ['Tons','Acre'], 'Dollars per ton': ['Dollars','Ton'], 'Cents per pound': ['Cents','Pound'], 'Dollars per short ton': ['Dollars','Short ton'], 'Metric tons per hectare': ['Metric tons','Hectare']}
 
 
