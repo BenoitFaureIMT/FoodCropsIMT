259011fb9cbc5d0d3eb0bf0f1aee3a517afe1034
Benoit Faure <98880295+BenoitFaureIMT@users.noreply.github.com>
2022-11-13 22:30:20 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-dataset = pd.read_csv('FeedGrains.csv')
-tot = dataset.shape[0]
-width = 50
-for index, __ in dataset.iterrows():
-    percent = 100.0*index/tot
-    left = width*percent/100
-    right = width-left
-    print("\r[", '*'*int(left), ' '*int(right), "]", round(percent), '%', sep="", end="", flush=True)
